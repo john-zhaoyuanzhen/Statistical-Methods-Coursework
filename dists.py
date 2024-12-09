543d34f382e271b3119614644c366bf014dcc55f
yz929 <yz929@cam.ac.uk>
2024-12-09 02:12:46 +0000

    Dec 9

--- dists.py
+++ dists.py
@@ -1,28 +1,40 @@
-from scipy.stats import uniform, truncnorm, truncexpon, crystalball
+from numba_stats import uniform, truncnorm, truncexpon, crystalball
+import numba as nb
 import numpy as np
 
 
-# Define the truncated distributions with given parameters
+# Define the truncated distributions with given parameters using numba_stats
 def g_s(x, beta, m, mu, sigma):
-    x = np.clip(x, 0, 5) # Truncate x to [0, 5]
-    cb = crystalball(beta=beta, m=m, loc=mu, scale=sigma)
-    return cb.pdf(x) / (cb.cdf(5) - cb.cdf(0))
+    x = np.asarray(x, dtype=np.float64)
+    x = np.clip(x, 0., 5.) # Truncate x to [0, 5]
+    num = crystalball.pdf(x.reshape(-1), beta=beta, m=m, loc=mu, scale=sigma).reshape(x.shape)
+    denom = crystalball.cdf(5., beta=beta, m=m, loc=mu, scale=sigma) - crystalball.cdf(0., beta=beta, m=m, loc=mu, scale=sigma)
+    return num / denom
 
-def h_s(x, lmda):
-    return truncexpon.pdf(x, b=10*lmda, loc=0, scale=1/lmda)
+def h_s(x, lmda, xmin=0., xmax=10.):
+    x = np.asarray(x, dtype=np.float64)
+    return truncexpon.pdf(x, xmin=xmin, xmax=xmax, loc=0., scale=1/lmda)
 
 def g_b(x):
-    return uniform.pdf(x, scale=5)
+    x = np.asarray(x, dtype=np.float64)
+    return uniform.pdf(x, a = 0., w = 5.)
 
 def h_b(x, mu_b, sigma_b):
-    return truncnorm.pdf(x, a=(0-mu_b)/sigma_b, b=(10-mu_b)/sigma_b, loc=mu_b, scale=sigma_b)
+    x = np.asarray(x, dtype=np.float64)
+    return truncnorm.pdf(x, xmin=0., xmax=10., loc=mu_b, scale=sigma_b)
 
 # Joint density function
 def s_xy(y, x, beta, m, mu, sigma, lmda):
+    x = np.asarray(x, dtype=np.float64)
+    y = np.asarray(y, dtype=np.float64)
     return g_s(x, beta, m, mu, sigma) * h_s(y, lmda)
 
 def b_xy(y, x, mu_b, sigma_b):
+    x = np.asarray(x, dtype=np.float64)
+    y = np.asarray(y, dtype=np.float64)
     return g_b(x) * h_b(y, mu_b, sigma_b)
 
 def f_xy(y, x, mu, sigma, beta, m, f, lmda, mu_b, sigma_b):
-    return f * g_s(x, beta, m, mu, sigma) * h_s(y, lmda) + (1 - f) * g_b(x) * h_b(y, mu_b, sigma_b)
+    x = np.asarray(x, dtype=np.float64)
+    y = np.asarray(y, dtype=np.float64)
+    return f * g_s(x, beta, m, mu, sigma) * h_s(y, lmda) + (1. - f) * g_b(x) * h_b(y, mu_b, sigma_b)
